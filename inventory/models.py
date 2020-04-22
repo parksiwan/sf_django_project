9853c753201209cabe5f2fed9b9d36c68809cf4d
siwan <parksiwan@gmail.com>
2020-04-22 22:33:28 +1000

    daily add

--- inventory/models.py
+++ inventory/models.py
@@ -43,22 +43,19 @@ class Stock(models.Model):
     inward = models.DateField(null=True)
     product_name = models.CharField(max_length=300, null=True)
     new_balance = models.FloatField()
-    #unit_choices = (
-    #    ('PACK', 'PACK'),
-    #    ('CTN', 'CTN'),
-    #)
+    
     unit = models.CharField(max_length=20)
     bbd = models.DateField(null=True)
     # Storage location
     location = models.CharField(max_length=20, null=True)
     origin = models.CharField(max_length=10, null=True)
     product_name_jp = models.CharField(max_length=300, null=True)
-    #bbd = models.DateField(null=True, blank=True)
+    
     #class Meta:
     #    abstract = True
     def __str__(self):
         return '({0}) {1}'.format(self.update_date, self.sf_code)
-        
+    
 class ProductList(models.Model):
     sf_code = models.CharField(max_length=20)
     product_type = models.CharField(max_length=10, null=True)
@@ -68,3 +65,39 @@ class NotUsedProductList(models.Model):
     sf_code = models.CharField(max_length=20)
     product_type = models.CharField(max_length=10, null=True)
     product_name = models.CharField(max_length=300, null=True)
+
+
+class DailyUsage(models.Model):
+    update_date = models.DateField()
+    product_type = models.CharField(max_length=10, null=True)
+    sf_code = models.CharField(max_length=20)
+    product_name = models.CharField(max_length=300, null=True)
+    pickup_qty = models.FloatField(null=True)
+    unit = models.CharField(max_length=20)#, choices=unit_choices)
+    memo = models.CharField(max_length=300)
+
+    origin = models.CharField(max_length=10, null=True)
+    product_name_jp = models.CharField(max_length=300, null=True)
+
+    def __str__(self):
+        return '({0}) {1} - {2} {3}'.format(self.update_date, self.sf_code, self.pickup_qty, self.unit)
+
+class DailyStock(models.Model):
+    update_date = models.DateField()
+    product_type = models.CharField(max_length=10, null=True)
+    sf_code = models.CharField(max_length=20)
+    inward = models.DateField(null=True)
+    product_name = models.CharField(max_length=300, null=True)
+    new_balance = models.FloatField()
+    
+    unit = models.CharField(max_length=20)
+    bbd = models.DateField(null=True)
+    # Storage location
+    location = models.CharField(max_length=20, null=True)
+    origin = models.CharField(max_length=10, null=True)
+    product_name_jp = models.CharField(max_length=300, null=True)
+    
+    #class Meta:
+    #    abstract = True
+    def __str__(self):
+        return '({0}) {1}'.format(self.update_date, self.sf_code)
