3ed24c8ca314c836dd86c89e812a9ad3d31a9ead
siwan <parksiwan@gmail.com>
2021-02-24 21:39:18 +1100

    24/2/2021

--- plenus/models.py
+++ plenus/models.py
@@ -10,7 +10,7 @@ class MonthlyUsage(models.Model):
     product_name = models.CharField(max_length=300, null=True)
     description = models.CharField(max_length=300, null=True)
     qty = models.FloatField(null=True)
-    unit = models.CharField(max_length=20)#, choices=unit_choices)    
+    unit = models.CharField(max_length=20)#, choices=unit_choices)
 
     class Meta:
         verbose_name = "Plenus Monthly Usage"
@@ -20,14 +20,14 @@ class MonthlyUsage(models.Model):
 class Stock(models.Model):
     stock_base_date = models.DateField()
     product_type = models.CharField(max_length=10, null=True)
-    sf_code = models.CharField(max_length=20)    
+    sf_code = models.CharField(max_length=20)
     plenus_code = models.CharField(max_length=20)
     product_name = models.CharField(max_length=300, null=True)
     description = models.CharField(max_length=300, null=True)
-    qty = models.FloatField()    
+    qty = models.FloatField()
     unit = models.CharField(max_length=20)
     bbd = models.DateField(null=True)
-            
+
     class Meta:
         verbose_name = "Plenus Stock"
         verbose_name_plural = "Plenus Stocks"
@@ -44,24 +44,22 @@ class CKStock(models.Model):
     pallet = models.CharField(max_length=20)
     sf_code = models.CharField(max_length=20)
 
-    product_type = models.CharField(max_length=10, null=True)    
+    product_type = models.CharField(max_length=10, null=True)
     product_name = models.CharField(max_length=300, null=True)
     description = models.CharField(max_length=300, null=True)
-        
+
     box_qty = models.FloatField(null=True, blank=True)  # CTN only
     pallet_qty = models.FloatField(null=True)
 
     bbd = models.DateField(null=True)
     pickup_by_choices = (
         ('NA', 'N/A'),
-        ('CTN', 'OUT'),
+        ('CTN', 'CTN'),
         ('PACK', 'PACK')
     )
     pickup_by = models.CharField(max_length=20, choices=pickup_by_choices, default='CTN')
     memo = models.CharField(max_length=300, null=True)
-    
+
     class Meta:
         verbose_name = "CK Stock Transaction"
         verbose_name_plural = "CK Stock Transactions"
-
-    
